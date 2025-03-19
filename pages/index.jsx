eff269737748e5acb873aab9446fb29dc44239fa
dido <deyanp19@hotmail.com>
2025-03-20 01:40:07 +0200

    first development commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,5 @@
-import Jumbotron from '@/components/jumbotron/Jumbotron';
-import AccordionSidebar from '@/components/sidebar/AccordionSidebar';
-// import Witget from '@/components/progressSteps/Witget';
-import SidebarSubmenu from '@/components/sidebar/SidebarSubmenu';
-import Home from '../src/components/blog/shared-theme/page';
+
+import Home from '@/components/blog/shared-theme/page';
 import Blog from './blog/Blog';
 
 
@@ -10,8 +7,7 @@ export default function Index() {
     console.log(Home)
 	return (
 		<div >
-			 {/* <SidebarSubmenu name="J D"/> */}
-			 {/* <AccordionSidebar/> */}
+			
 			 <Blog />
 			  
 		</div>
