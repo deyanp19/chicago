86a91d82aead8c783609ba4c35a34d8b446c0db1
deyanp19 <deyanp19@hotmail.com>
2025-11-12 16:16:35 -0600

    restructuring index.js and pulling out the logic for logging into files

--- back_end/index.js
+++ back_end/index.js
@@ -1,19 +1,28 @@
 const winston = require('winston')
 const express = require('express');
 const app = express();
-const config = require('config');
+const cors = require('cors');
+
+// winston.add(new winston.transports.File({filename:'logfile.log'}));
+// winston.add(new winston.transports.MongoDB({db:'mongodb://localhost:21017/vidly', collection:'logs-barlogs', capped: true, metaKey: 'meta'}));
+
+if (!config.get('jwtPrivateKey')) {
+  console.error('FATAL ERROR: Backend app not authenticated jwt not present');
+  process.exit(1);
+}
+
+mongoose.connect('mongodb://localhost:27017/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...',err));
+
+app.use(cors()); // Or: app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(express.json());
+app.use('/api/auth', auth);
+app.use('/api/users/', users);
+
+//app.use(error);// not calling the function error, just referencing
 
 
-require('./startup/logging')();
-require('./startup/routes')(app);
-require('./startup/db')();
-require('./startup/config')();
-require('./startup/validation')();
-require('./startup/prod')(app);
 
- 
-
-const port = process.env.PORT ||  config.get('PORT');
-const server = app.listen(port, () => winston.info(`Backend App is listening on port ${port}...`)); 
-
-module.exports = server;
+const port = process.env.PORT || config.get('PORT');
+app.listen(port, () => console.log(`Backend App is listening on port ${port}...`));
