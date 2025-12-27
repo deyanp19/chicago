aa13c763cb3e177423912de79a159184e1b54140
deyanp19 <deyanp19@hotmail.com>
2025-12-27 21:18:48 +0200

    using mui list to fix rendering in mobile chrome

--- pages/profile/page_components/ProfileInformation.jsx
+++ pages/profile/page_components/ProfileInformation.jsx
@@ -1,7 +1,9 @@
 import { AuthContext } from "@/context/AuthContext";
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, ListItemText } from "@mui/material";
 import { useContext } from "react";
 import AppTheme from '@components/AppTheme';
+import List from '@mui/material/List';
+import ListItem from "@mui/material/ListItem";
 
 
 export default function ProfileInformation(props) {
@@ -11,15 +13,17 @@ export default function ProfileInformation(props) {
         <AppTheme {...props} >
             <CssBaseline enableColorScheme />
         <div>
-            {/* <h1>Hello {user.name}</h1>
-            <p>Is logged in now: <b>{isLoggedIn ? "True" : "False"}</b></p>
+            <h1>Hello {user.name}</h1>
+             <p>Is logged in now: <b>{isLoggedIn ? "True" : "False"}</b></p>
             <p>User Information:</p>
-            <ul>
-                <li>ID:  {user.id}</li>
-                <li><b>email: </b>{user.email}</li>
+           <List component="ol"> 
 
-                <li><b>name: </b>{user.name}</li>
-            </ul> */}
+                <ListItemText primary={`ID: ${user.id}`} />
+                <ListItemText primary={`name: ${user.name}`} />
+                <ListItemText primary={`email: ${user.email}`} />
+             
+           </List>
+            
         </div>
         </AppTheme>
     );
